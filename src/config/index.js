5e837b4a5d47d34f0bd4c9c2181c54f0f598fdf6
anil babu <anilbabu3245@gmail.com>
2024-10-31 17:26:15 +0530

    df

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,5 @@
-export const BASE_URL = "http://localhost:8000/api/";
+// export const BASE_URL = "http://localhost:8000/api/";
+
+export const BASE_URL = "https://dee0-2409-40d2-10ef-5f41-9094-8189-de38-34a5.ngrok-free.app/api/";
+
+
